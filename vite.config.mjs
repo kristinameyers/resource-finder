9637932ac1011132d6a82ee7923aa77b475b591b
kristinameyers <meyers.kristina@gmail.com>
2025-09-08 17:01:37 -0700

    disabling the theme plugin to resolve the ENOENT error

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
+// import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
 import { fileURLToPath } from "url";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
@@ -11,7 +11,6 @@ export default defineConfig({
   plugins: [
     react(),
     runtimeErrorOverlay(),
-    themePlugin(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
